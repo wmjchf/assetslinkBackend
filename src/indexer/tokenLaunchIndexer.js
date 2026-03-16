a274a84afbcc30a60d7324459b1ffbf05eb172e6
wmjchf <3284304056@qq.com>
2026-03-16 19:28:09 +0800

    feat: 更新

--- src/indexer/tokenLaunchIndexer.js
+++ src/indexer/tokenLaunchIndexer.js
@@ -232,8 +232,7 @@ async function main() {
         fromBlock: BigInt(from),
         toBlock: BigInt(to),
       });
-      console.log(logs,'logs',from,to);
-
+console.log(logs,'logs',from,to,factoryAddress);
       // Sort for deterministic processing
       logs.sort((a, b) => {
         if (a.blockNumber !== b.blockNumber) return Number(a.blockNumber - b.blockNumber);
